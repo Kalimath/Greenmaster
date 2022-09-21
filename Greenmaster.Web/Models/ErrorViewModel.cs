a84b976936a583707c72934938357a38d71ddf03
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2022-09-21 13:33:34 +0200

    rebase conflicts merged

---
+++ Greenmaster.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Greenmaster.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
