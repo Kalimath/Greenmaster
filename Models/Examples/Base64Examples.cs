6c467abd6adec882a822ef16f0946490292c8c18
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2023-03-23 17:20:56 +0100

    Added Resize method

--- Models/Examples/Base64Examples.cs
+++ Models/Examples/Base64Examples.cs
@@ -3,10 +3,7 @@
 namespace Greenmaster_ASP.Models.Examples{
 public static class Base64Examples
   {
-      private const string PathImageSpecie = @"C:\Users\mathi\RiderProjects\Greenmaster_ASP\Greenmaster_ASP\wwwroot\media\img\Arboretum\Trees0091_350.jpg";
-      private const string PathImageRendering = @"C:\Users\mathi\RiderProjects\Greenmaster_ASP\Greenmaster_ASP\wwwroot\media\clipart\treetops\top_tree_2-250px.png";
-      
-      public static string ImageSpecie = FileConverter.ToBase64(PathImageSpecie);
-      public static string ImageRendering = FileConverter.ToBase64(PathImageRendering);
+      public static string ImageSpecie = FileConverter.ToBase64(PathExamples.PathImageSpecie);
+      public static string ImageRendering = FileConverter.ToBase64(PathExamples.PathImageRendering);
   }
  }
---
+++ Tests/Helpers/Converters/ImageConverterTests/ResizeShould.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using Greenmaster_ASP.Models.Examples;
+using Xunit;
+using ImageConverter = Greenmaster_ASP.Helpers.ImageConverter;
+
+namespace Greenmaster_ASP.Tests.Helpers.Converters.ImageConverterTests;
+
+public class ResizeShould
+{
+    
+    [Fact]
+    public void ReturnImageWithPassedHeight()
+    {
+        Image initialImage = new Bitmap(PathExamples.PathImageRendering);
+        const int height = 150;
+
+        var resizedImage = ImageConverter.Resize(initialImage, 130, height);
+        
+        Assert.Equal(height, resizedImage.Height);
+    }
+    
+    [Fact]
+    public void ReturnImageWithPassedWidth()
+    {
+        Image initialImage = new Bitmap(PathExamples.PathImageRendering);
+        const int width = 130;
+        
+        var resizedImage = ImageConverter.Resize(initialImage, width, 150);
+        
+        Assert.Equal(width, resizedImage.Width);
+    }
+}
