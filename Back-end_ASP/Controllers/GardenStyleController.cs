78bff75612a089794b5086fdf545b1bd1023aab6
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2023-05-21 14:46:45 +0200

    GardenStyleController added

---
+++ Back-end_ASP/Controllers/GardenStyleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Greenmaster_ASP.Controllers;
+
+public class GardenStyleController : Controller
+{
+    public IActionResult Index()
+    {
+        throw new NotImplementedException();
+    }
+}
