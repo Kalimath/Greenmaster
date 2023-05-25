381ae308219e3823d3eeaa490cef5c80e4ca984e
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2023-05-26 00:17:33 +0200

    Added migrations folder

---
+++ Back-end_ASP/Migrations/20230523173136_Colors reworked.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Greenmaster_ASP.Migrations
+{
+    public partial class Colorsreworked : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GardenStyles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Colors",
+                value: new[] { "Color [White]", "Color [Green]", "Color [Blue]" });
+
+            migrationBuilder.UpdateData(
+                table: "Species",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FlowerColors",
+                value: "Color [Blue],Color [Orange]");
+
+            migrationBuilder.UpdateData(
+                table: "Species",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FlowerColors",
+                value: "Color [Pink]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GardenStyles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Colors",
+                value: new[] { "White", "Green", "Blue" });
+
+            migrationBuilder.UpdateData(
+                table: "Species",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FlowerColors",
+                value: "Blue,MultiColor,Orange");
+
+            migrationBuilder.UpdateData(
+                table: "Species",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FlowerColors",
+                value: "Pink");
+        }
+    }
+}
---
+++ Back-end_ASP/Migrations/20230525205028_AddedPathSizeAndMaterialTypes.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Greenmaster_ASP.Migrations
+{
+    public partial class AddedPathSizeAndMaterialTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PathSize",
+                table: "GardenStyles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "MaterialTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    GardenStyleId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaterialTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MaterialTypes_GardenStyles_GardenStyleId",
+                        column: x => x.GardenStyleId,
+                        principalTable: "GardenStyles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialTypes_GardenStyleId",
+                table: "MaterialTypes",
+                column: "GardenStyleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MaterialTypes");
+
+            migrationBuilder.DropColumn(
+                name: "PathSize",
+                table: "GardenStyles");
+        }
+    }
+}
