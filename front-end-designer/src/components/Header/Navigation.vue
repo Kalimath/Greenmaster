0df95ca2f0c78e16b357d8d567f16ce8e4bb791c
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2023-06-20 21:13:53 +0200

    Fixed Tailwind framework

---
+++ front-end-designer/src/components/Header/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Species</li>
+      <li>Materials</li>
+      <li>Styles</li>
+    </ul>
+  </nav>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+}
+</script>
+
+<style scoped>
+
+</style>
