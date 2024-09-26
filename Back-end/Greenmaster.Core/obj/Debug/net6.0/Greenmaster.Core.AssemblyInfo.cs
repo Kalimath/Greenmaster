2c6420f0c561b223d41e261ac61009843537eea8
mathieu <mathieu_broe@yahoo.com>
2024-09-26 22:36:10 +0200

    Revert "reverted"
    This reverts commit fa9260818b0294cb7f7e3be17bc2d7fa7c93e92f.

--- Back-end/Greenmaster.Core/obj/Debug/net6.0/Greenmaster.Core.AssemblyInfo.cs
+++ Back-end/Greenmaster.Core/obj/Debug/net6.0/Greenmaster.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Greenmaster.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50cc06743974e1f08742feb680fa37fe51142bad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Greenmaster.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Greenmaster.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
